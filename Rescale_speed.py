a9a7579a56c9be623433e3399a328ead93c29eda
Ismael El Kasbaoui <ismo67nhf@hotmail.fr>
2019-11-25 10:57:14 +0100

    working

--- Rescale_speed.py
+++ Rescale_speed.py
@@ -1,47 +1,87 @@
+## Imports
+import sys
+import random
+import math
+import os,errno
+import glob
+import pathlib
+import shutil
+import numpy as np
+import copy
+import cv2
+
+
 # delete speed
-import re
+
+
+# new_path = "/content/drive/My Drive/Air-D/Unet-5/dataset_images" 
+
+TrainP ="C:\\Work\Air-D\\pour_marouane\\dataset_images\\dataset_images"
+
+quartier_id  = next(os.walk(TrainP))[1]
+print(len(quartier_id) )
+
+
+# def delete_speed(quartier_id) :
+#         for p in quartier_id:
+#             spd = os.path.join(TrainP,p,"X","speed.png")
+#             if os.path.exists(spd) : 
+#                  os.remove(spd)
+#                  print(" file {} remove ".format(spd))
+#             else:
+#                 continue
+
 
 def Deletedir(path):
    shutil.rmtree(path)
 
+# AlreadyDone = []
 
-def Rescale_speed(quartier_id):
+delete_speed(quartier_id)
 
-        AlreadyDone = []
-        for Q in quartier_id :
-            x_path = os.path.join(TestP, Q)
-            
-            split = Q.split('_')
-
-            if split[4] == "rotated" :
-                Dir = split[7]
-                Vit = split[9]
-                Rot = split[5]
-            else :
-                Dir = split[5]
-                Vit = split[7]
-                Rot = 0
-            
-            if (Dir,Rot) in AlreadyDone:
-            Deletedir(x_path)
-
-            else:
-            #  os.path.join(self.path, id_name,"Y","pollutionMap") + ".png"
-                im_path = os.path.join(x_path, "Y", "pollutionMap.png")
-                image_vit = cv2.imread(im_path)
-                speed = image_vit * 1.5/float(Vit)
-                cv2.imwrite(im_path, speed)
-                AlreadyDone.append((Dir,Rot))    
+for Q in quartier_id:
+      
+     x_path = os.path.join(TrainP, Q)
+     split = Q.split('_')
+     name_quartier = split[3]
 
 
+     if split[4] == "rotated" :
+        Dir = split[7]
+        Vit = split[9]
+        Rot = split[5]
+     else :
+        Dir = split[5]
+        Vit = split[7]
+        Rot = 0
+     
+     if (name_quartier, Dir, Rot) in AlreadyDone:
 
+        Deletedir(x_path)
+        print(" Remove ... {}".format(Q))
 
-if __name__=if __name__ == "__main__":
+     else:
+        im_path = os.path.join(x_path, "Y", "pollutionMap")
+        
+        try:
 
-        TrainP = "/content/drive/My Drive/Air-D/Unet-5/dataset_images/train"
-        TestP = "/content/drive/My Drive/Air-D/Unet-5/dataset_images/test"
+            if os.path.exists(im_path+".png"):
+                  image_vit = cv2.imread(im_path+".png",cv2.IMREAD_GRAYSCALE)
 
-        quartier_id  = next(os.walk(TestP))[1]
+                  cv2.imshow(Q,image_vit)
+                  print(" ")
 
+                  speed = image_vit * float(Vit)/1.5
+                  speed = np.around(speed)
+                  speed = speed.astype(np.uint8)
 
-  
+                #   cv2.imshow('transformed',speed)
+                  cv2.imwrite(im_path+".png", speed)
+                  
+                  print(" ")
+                  if not cv2.imwrite(im_path+".png", speed):
+                        raise Exception("Could not write image")
+
+                  AlreadyDone.append((name_quartier,Dir,Rot))      
+        except : 
+            print(" image {} does not exist ".format(im_path))
